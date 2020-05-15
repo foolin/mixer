8281b206b29c8d2991b616203208e8d2c93984d6
foolin <foolin@126.com>
2020-05-15 23:56:18 +0800

    add ignore

--- lcg.go
+++ lcg.go
@@ -53,9 +53,7 @@ func (g *LCGRandom) Perm(n int) []int {
 	m := make([]int, n)
 	// In the following loop, the iteration when i=0 always swaps m[0] with m[0].
 	// A change to remove this useless iteration is to assign 1 to i in the init
-	// statement. But Perm also effects r. Making this change will affect
-	// the final state of r. So this change can't be made for compatibility
-	// reasons for Go 1.
+	// statement.
 	for i := 0; i < n; i++ {
 		j := g.Intn(i + 1)
 		m[i] = m[j]
