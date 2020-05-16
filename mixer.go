0b38fcc84f982995c4061b831a5cf75fb3959276
foolin <foolin@126.com>
2020-05-16 21:34:05 +0800

    change method.

--- mixer.go
+++ mixer.go
@@ -14,7 +14,7 @@ const (
 var (
 
 	//StdMixer
-	StdMixer = AlphanumericLowerMixer
+	StdMixer = AlphanumericCaseMixer
 
 	//AlphanumericCaseMixer the alphanumeric include upper and lower:`0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`
 	AlphanumericCaseMixer = MustNewWith(defaultSalt, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
