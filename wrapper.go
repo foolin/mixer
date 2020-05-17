106d4a8031c77869e595d9baa6a3eaf22bdda2a2
foolin <foolin@126.com>
2020-05-17 18:34:08 +0800

    update test

--- wrapper.go
+++ wrapper.go
@@ -1,59 +1,59 @@
 package mixer
 
-var globalStdStringMixer = AlphanumericCaseMixer
-var globalStdNumberMixer = AlphanumericUpperMixer
+var globalStdCaseMixer = AlphanumericCaseMixer
+var globalStdUpperMixer = AlphanumericUpperMixer
 
 //EncodeString encode string use global default mixer
 func EncodeString(password string, data string) string {
-	return globalStdStringMixer.EncodeString(password, data)
+	return globalStdCaseMixer.EncodeString(password, data)
 }
 
 //DecodeString encode global default mixer
 func DecodeString(password string, data string) string {
-	return globalStdStringMixer.DecodeString(password, data)
+	return globalStdCaseMixer.DecodeString(password, data)
 }
 
 //EncodeNumber encode global default mixer
 func EncodeNumber(password string, value int64) string {
-	return globalStdNumberMixer.EncodeNumber(password, value)
+	return globalStdUpperMixer.EncodeNumber(password, value)
 }
 
 //EncodeNumberPadding  encode padding default number mixer
 func EncodeNumberPadding(password string, value int64, paddingLen int) string {
-	return globalStdNumberMixer.EncodeNumberPadding(password, value, paddingLen)
+	return globalStdUpperMixer.EncodeNumberPadding(password, value, paddingLen)
 }
 
 //DecodeNumber decode default number mixer
 func DecodeNumber(password string, data string) (int64, error) {
-	return globalStdNumberMixer.DecodeNumber(password, data)
+	return globalStdUpperMixer.DecodeNumber(password, data)
 }
 
 //EncodeID encode global default mixer
 func EncodeID(password string, value uint64) string {
-	return globalStdNumberMixer.EncodeID(password, value)
+	return globalStdUpperMixer.EncodeID(password, value)
 }
 
 //EncodeIDPadding  encode padding default number mixer
 func EncodeIDPadding(password string, value uint64, paddingLen int) string {
-	return globalStdNumberMixer.EncodeIDPadding(password, value, paddingLen)
+	return globalStdUpperMixer.EncodeIDPadding(password, value, paddingLen)
 }
 
 //DecodeID decode default number mixer
 func DecodeID(password string, data string) (uint64, error) {
-	return globalStdNumberMixer.DecodeID(password, data)
+	return globalStdUpperMixer.DecodeID(password, data)
 }
 
 //EncodeBase32 encode global default mixer
 func EncodeBase32(password string, value string) string {
-	return globalStdNumberMixer.EncodeBase32(password, value)
+	return globalStdUpperMixer.EncodeBase32(password, value)
 }
 
 //EncodeBase32Padding  encode padding default number mixer
 func EncodeBase32Padding(password string, value string, paddingLen int) string {
-	return globalStdNumberMixer.EncodeBase32Padding(password, value, paddingLen)
+	return globalStdUpperMixer.EncodeBase32Padding(password, value, paddingLen)
 }
 
 //DecodeBase32 decode default number mixer
 func DecodeBase32(password string, data string) (string, error) {
-	return globalStdNumberMixer.DecodeBase32(password, data)
+	return globalStdUpperMixer.DecodeBase32(password, data)
 }
